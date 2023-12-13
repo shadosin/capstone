6f55f599c9d4766612e0e140bb6a06626aed71ae
joesawyer <joseph.sawyer@mykenzie.snhu.edu>
2023-12-13 09:17:46 -0600

    Correct the name of CreateUserRequest file User record, request, response, update classes

--- Application/src/main/java/com/kenzie/appserver/controller/model/UserResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/UserResponse.java
@@ -1,4 +1,116 @@
 package com.kenzie.appserver.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kenzie.appserver.repositories.model.UserRecord;
+
+import javax.validation.constraints.NotEmpty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
+    @NotEmpty
+    @JsonProperty("userId")
+    private String userId;
+    @JsonProperty("userName")
+    private String userName;
+    @JsonProperty("firstName")
+    private String firstName;
+    @JsonProperty("lastName")
+    private String lastName;
+    @JsonProperty("birthDate")
+    private String birthDate;
+    @JsonProperty("address")
+    private String address;
+    @JsonProperty("phoneNum")
+    private String phoneNum;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("dateJoined")
+    private String dateJoined;
+
+    public UserResponse() {}
+
+    public UserResponse(UserRecord userRecord) {
+        this.userId = userRecord.getUserId();
+        this.userName = userRecord.getUsername();
+        this.firstName = userRecord.getFirstName();
+        this.lastName = userRecord.getLastName();
+        this.birthDate = userRecord.getBirthDate();
+        this.address = userRecord.getAddress();
+        this.phoneNum = userRecord.getPhoneNum();
+        this.email = userRecord.getEmail();
+        this.dateJoined = userRecord.getDateJoined();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(String dateJoined) {
+        this.dateJoined = dateJoined;
+    }
 }
--- Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
@@ -1,7 +1,122 @@
 package com.kenzie.appserver.repositories.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
 
 @DynamoDBTable(tableName = "Users")
 public class UserRecord {
+    private String userId;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String address;
+    private String phoneNum;
+    private String email;
+    private String dateJoined;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    @DynamoDBAttribute(attributeName = "firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @DynamoDBAttribute(attributeName = "lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    @DynamoDBAttribute(attributeName = "birthDate")
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    @DynamoDBAttribute(attributeName = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    @DynamoDBAttribute(attributeName = "phoneNum")
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    @DynamoDBAttribute(attributeName = "dateJoined")
+    public String getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDateJoined(String dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRecord that = (UserRecord) o;
+        return Objects.equals(userId, that.userId)
+                && Objects.equals(username, that.username)
+                && Objects.equals(firstName, that.firstName)
+                && Objects.equals(lastName, that.lastName)
+                && Objects.equals(birthDate, that.birthDate)
+                && Objects.equals(address, that.address)
+                && Objects.equals(phoneNum, that.phoneNum)
+                && Objects.equals(email, that.email)
+                && Objects.equals(dateJoined, that.dateJoined);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, firstName, lastName, birthDate, address, phoneNum, email, dateJoined);
+    }
 }
