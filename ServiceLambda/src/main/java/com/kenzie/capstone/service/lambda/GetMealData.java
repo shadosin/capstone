4e043c74f9073cd7761c656297a4be2aee803c32
beangel128 <Benjamin.angel@mykenzie.snhu.edu>
2023-12-16 21:42:54 -0600

    GetMeal lambda implemented

---
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/GetMealData.java
@@ -0,0 +1,57 @@
+package com.kenzie.capstone.service.lambda;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.kenzie.capstone.service.dependency.ServiceComponent;
+import com.kenzie.capstone.service.model.MealData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetMealData implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    static final Logger log = LogManager.getLogger();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        log.info(gson.toJson(input));
+
+        ServiceComponent serviceComponent = DaggerServiceComponent.create();
+        MealService service = serviceComponent.provideMealService;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        String id = input.getPathParameters().get("id");
+
+        if (id == null || id.length() == 0) {
+            return response
+                    .withStatusCode(400)
+                    .withBody("Id is invalid");
+        }
+
+        try {
+            MealData mealData = service.getMealData(id);
+            String output = gson.toJson(mealData);
+
+            return response
+                    .withStatusCode(200)
+                    .withBody(output);
+
+        } catch (Exception e) {
+            return response
+                    .withStatusCode(400)
+                    .withBody(gson.toJson(e.getMessage()));
+        }
+    }
+}
+
