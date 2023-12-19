ffaf24ea8dce2925c9fc02f4a6f50613805cf218
joesawyer <joseph.sawyer@mykenzie.snhu.edu>
2023-12-19 17:51:21 -0600

    Start changes to create method

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/ServiceModule.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/ServiceModule.java
@@ -2,6 +2,7 @@ package com.kenzie.capstone.service.dependency;
 
 import com.kenzie.capstone.service.ExerciseService;
 import com.kenzie.capstone.service.LambdaService;
+import com.kenzie.capstone.service.MealService;
 import com.kenzie.capstone.service.dao.ExampleDao;
 
 import com.kenzie.capstone.service.dao.ExerciseDao;
