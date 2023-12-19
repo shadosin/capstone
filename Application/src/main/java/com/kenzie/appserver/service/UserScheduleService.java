43f13945b01ad93c2d0f610b4b77f55f0b8209ba
joesawyer <joseph.sawyer@mykenzie.snhu.edu>
2023-12-19 11:38:01 -0600

    UserSchedule service

--- Application/src/main/java/com/kenzie/appserver/service/UserScheduleService.java
+++ Application/src/main/java/com/kenzie/appserver/service/UserScheduleService.java
@@ -1,4 +1,36 @@
 package com.kenzie.appserver.service;
 
+import com.kenzie.appserver.repositories.UserScheduleRepository;
+import com.kenzie.appserver.repositories.model.UserRecord;
+import com.kenzie.appserver.repositories.model.UserScheduleRecord;
+import com.kenzie.appserver.service.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserScheduleService {
+
+    private final UserScheduleRepository userScheduleRepository;
+    private final UserService userService;
+
+    @Autowired
+    public UserScheduleService(UserScheduleRepository userScheduleRepository, UserService userService) {
+        this.userScheduleRepository = userScheduleRepository;
+        this.userService = userService;
+    }
+
+    public UserScheduleRecord findById(String id) {
+        return userScheduleRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No record found for ID: " + id));
+    }
+
+    public UserScheduleRecord findCurrentSchedule(String userId) {
+        UserRecord userRecord = userService.findById(userId);
+        if (userRecord == null) {
+            throw new IllegalArgumentException("User does not exist with ID: " + userId);
+        }
+        List<String> userSchedules = userRecord.
+    }
 }
