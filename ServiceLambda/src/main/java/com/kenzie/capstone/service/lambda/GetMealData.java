01bfdf84819b9a0a6cec10b1e591e825e2a8f159
beangel128 <Benjamin.angel@mykenzie.snhu.edu>
2023-12-19 19:15:34 -0600

    final push

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/GetMealData.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/GetMealData.java
@@ -6,12 +6,14 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.kenzie.capstone.service.MealService;
+import com.kenzie.capstone.service.dependency.DaggerServiceComponent;
 import com.kenzie.capstone.service.dependency.ServiceComponent;
 import com.kenzie.capstone.service.model.MealData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class GetMealData implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -25,7 +27,7 @@ public class GetMealData implements RequestHandler<APIGatewayProxyRequestEvent, 
         log.info(gson.toJson(input));
 
         ServiceComponent serviceComponent = DaggerServiceComponent.create();
-        MealService service = serviceComponent.provideMealService;
+        MealService service = serviceComponent.provideMealService();
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
