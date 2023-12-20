d095713766a54fad87f2433d98479be728ce49f5
joesawyer <joseph.sawyer@mykenzie.snhu.edu>
2023-12-20 08:03:49 -0600

    Create ScheduledEvent Controller, Service, Record, requests, response, converter, repository

--- Application/src/main/java/com/kenzie/appserver/service/UserService.java
+++ Application/src/main/java/com/kenzie/appserver/service/UserService.java
@@ -13,6 +13,7 @@ public class UserService {
 
   private final UserRepository userRepository;
 
+  //TODO - why LambdaServiceclient is here?
   @Autowired
   public UserService(UserRepository userRepository, LambdaServiceClient lambdaServiceClient) {
     this.userRepository = userRepository;
