82f586f631d43a56712fcfda64e5e1c23a2678b8
John Olson <john.olson@mykenzie.snhu.edu>
2024-01-02 07:18:08 -0600

    Refactor login UI and navigation system
    Reformatted the login form creation in login.js for better structure and maintainability. Updated visual styles in style.css and the navigation system in navbar.js for a more intuitive user experience. This also included dynamically loading pages based on user status in index.js, and implementing the user dashboard functionality in dashboard.js. These changes aimed to provide a more streamlined and user-friendly application experience.

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,74 +1,82 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 module.exports = {
-
-  mode: 'development',
+  mode: "production",
   entry: {
-    bundle: path.resolve(__dirname, "src/index.js")
-
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
     clean: true,
-    assetModuleFilename: '[name][ext]',
+    assetModuleFilename: "[name][ext]",
   },
-  devtool: 'source-map',
+  performance: {
+    hints: false,
+  },
+  devtool: "source-map",
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3030,
     open: false,
     hot: true,
     compress: true,
+    liveReload: false,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
-          {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules/,
-            include: path.resolve(__dirname, 'pages'),
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['@babel/preset-react', { targets: "defaults" }]
-                ],
-              }
-            }
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "pages"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-react", { targets: "defaults" }]],
           },
+        },
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'VitaTrac',
-      filename: 'index.html',
-      template: 'src/index.html'
+      title: "VitaTrac",
+      filename: "index.html",
+      template: "src/index.html",
     }),
     new CopyPlugin({
       patterns: [
-        {from: path.resolve(__dirname, 'src', 'manifest.json'), to: path.resolve(__dirname, 'dist', 'manifest.json')},
-        {from: path.resolve(__dirname, 'src', 'icons'), to: path.resolve(__dirname, 'dist', 'icons')}
-        
-      ]
+        {
+          from: path.resolve(__dirname, "src", "manifest.json"),
+          to: path.resolve(__dirname, "dist", "manifest.json"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "icons"),
+          to: path.resolve(__dirname, "dist", "icons"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "assets", "favicon.ico"),
+          to: path.resolve(__dirname, "dist", "favicon.ico"),
+        },
+      ],
     }),
-    new WorkboxWebpackPlugin.InjectManifest({
-      swSrc: path.resolve(__dirname, 'src', 'service-worker.js'),
-      swDest: path.resolve(__dirname, 'dist', 'service-worker.js')
-    })
-  ]
-}
+  ],
+};
 
-
-// need copy plugin syntax
+//new WorkboxWebpackPlugin.InjectManifest({
+//  swSrc: path.resolve(__dirname, 'src', 'service-worker.js'),
+//  swDest: path.resolve(__dirname, 'dist', 'service-worker.js'),
+//  maximumFileSizeToCacheInBytes: 5000000,
+//})
