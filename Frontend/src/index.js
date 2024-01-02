c771bec26db846f47553d60cd0464b7ff33bc663
John Olson <john.olson@mykenzie.snhu.edu>
2024-01-01 20:19:15 -0600

    Add login form creation to login.js and styles
    Added a function in login.js to create the login form dynamically. Updated various styles in style.css to improve the UI. Modified navbar.js to add register and login functionality. Also, made changes in index.js to load different pages dynamically based on user status. Introduced the dashboard.js to load user dashboard when the user is logged in.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -11,49 +11,26 @@ import navbar from "./navbar";
 
 import home from "./pages/home";
 import login from "./pages/login";
-
-const router = new Navigo("/");
-
-// router.navigate("/login");
-router.updatePageLinks();
-
-window.addEventListener("load", () => {
-  const render = (content) => {
-    console.log(content);
-    document.querySelector("main section").innerHTML = content;
-  };
-  router
-    .on("/", function () {
-      const content = home();
-      console.log(content);
-      render(content);
-    })
-    .on("/login", function () {
-      const content = login();
-      render(content);
-    });
-
-  router.notFound(function () {
-    const content = "<h1>404 Error</h1>";
-    render(content);
-  });
-
-  router.hooks({
-    before: (done, params) => {
-      console.log(params);
-      done();
-    },
-    after: (params) => {
-      console.log(params);
-    },
-  });
-});
+import dashboard from "./pages/dashboard";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log(`Document Loaded`);
   console.log(`Loading NavBar`);
   navbar();
+
+  const userInfo = JSON.parse(window.sessionStorage.getItem("userInfo"));
+
+  if (userInfo) {
+    // load dashboard page
+    dashboard();
+  } else {
+    home();
+  }
 });
+
+if (window.sessionStorage.getItem("userInfo")) {
+  console.log(JSON.parse(window.sessionStorage.getItem("userInfo")));
+}
 
 // const loginBtn = document.querySelector('#login')
 // loginBtn.addEventListener("click", () => router.navigate("login"))
--- Frontend/src/pages/login.js
+++ Frontend/src/pages/login.js
@@ -1,14 +1,83 @@
+import createPage from "../components/createPage";
+
+
+function submitForm(event) {
+   
+}
+
+function createLoginForm () {
+
+    
+    const form = document.createElement("form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const username = userInput.value;
+        const password = passwordInput.value;
+    
+        console.log(`Username ${username} \n Password: ${password}`)
+        // get user info from api
+        const user = {
+            username,
+            password
+        }
+        window.sessionStorage.setItem('userInfo', JSON.stringify(user))
+        window.location.href = "/"
+    })
+
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+  
+    const userLabel = document.createElement("label");
+    userLabel.setAttribute("for", "username");
+    userLabel.innerText = "UserName";
+  
+    const userInput = document.createElement("input");
+    userInput.setAttribute("type", "text");
+    userInput.setAttribute("id", "username");
+    userInput.setAttribute("name", "username");
+    userInput.setAttribute("placeholder", "Username");
+    userInput.setAttribute("required", "true");
+  
+    const passwordLabel = document.createElement("label");
+    passwordLabel.setAttribute("for", "password");
+    passwordLabel.innerText = "Password";
+  
+    const passwordInput = document.createElement("input");
+    passwordInput.setAttribute("type", "password");
+    passwordInput.setAttribute("id", "password");
+    passwordInput.setAttribute("name", "password");
+    passwordInput.setAttribute("placeholder", "Password");
+    passwordInput.setAttribute("required", "true");
+  
+    const buttonGroup = document.createElement("div");
+    buttonGroup.classList.add("btnGroup");
+  
+    const canceltBtn = document.createElement("button");
+    canceltBtn.innerText = `Cancel`;
+    canceltBtn.classList.add("outline", "secondary")
+    canceltBtn.addEventListener("click", () => {
+        window.location.href = '/';
+    })
+    const submitBtn = document.createElement("input");
+    submitBtn.setAttribute("type", "submit");
+    //   submitBtn.innerText = `Submit`;
+  
+    buttonGroup.append(canceltBtn, submitBtn);
+    form.append(userLabel, userInput, passwordLabel, passwordInput, buttonGroup);
+
+    return form;
+}
+
+
 export default function login() {
-  const section = document.querySelector("section");
+  createPage("Login");
+  const pageContent = document.querySelector("#pageContent");
+  const section = document.createElement("section");
+  section.classList.add("container");
+  section.append(createLoginForm());
+  pageContent.append(section);
 
-  const div = document.createElement("div");
-  div.classList.add("container");
-
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  card.innerHTML = `<h2>Login Page</h2>`;
-
-  div.append(card);
-  section.append(div);
+  
 }
